383d29dd48750db1bb10ac55d9c6562f5604718a
kumauma <sehyun1124@naver.com>
2025-08-16 17:57:32 +0900

    프로젝트 초기 설정: Gradle 설정 파일 및 기본 애플리케이션 구조 추가

---
+++ src/main/java/net/dsa/scitHub/ScitHubApplication.java
@@ -0,0 +1,13 @@
+package net.dsa.scitHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScitHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/dsa/scitHub/ScitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dsa.scitHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
