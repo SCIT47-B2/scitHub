2b023e4f814d9c4628864188a68290b119e8564d
ysaen <yysaeeun@gmail.com>
2025-09-10 15:43:16 +0900

    refactor: Remove unused imports in CompanyDTO and CompanyService; adjust dropdown menu margin in archive.css

--- src/main/java/net/dsa/scitHub/dto/CompanyDTO.java
+++ src/main/java/net/dsa/scitHub/dto/CompanyDTO.java
@@ -1,6 +1,5 @@
 package net.dsa.scitHub.dto;
 
-import java.util.List;
 import java.util.OptionalDouble;
 
 import lombok.AllArgsConstructor;
--- src/main/java/net/dsa/scitHub/service/CompanyService.java
+++ src/main/java/net/dsa/scitHub/service/CompanyService.java
@@ -5,13 +5,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 
