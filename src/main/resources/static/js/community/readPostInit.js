b5146d34a1860999d2dc3e2564c07b6c994efac2
SEO_JAESIK <jaesik495@gmail.com>
2025-09-24 20:16:30 +0900

    refactor: 게시판 URL에 현재 페이지와 검색 범위, 검색어 정보를 담도록 변경
    🟠우선순위 중
    
    - [x]  게시판에서 검색 후에 새로고침을 하면 검색하지 않은 상태로 돌아감 (URL에 검색 관련 속성이 들어가도록 수정해서 해결)
    - [x]  검색 수행 후 특정 게시글에 접속하고 뒤로가기 버튼을 눌러 이동했을 때, 검색 결과가 보이지 않음 (History API를 이용한 이전 URL 분기 처리로 해결)
    
    🟢우선순위 하
    
    - [x]  1번 페이지가 아닌 페이지에서 글을 하나 보고 뒤로 가면 1번 페이지로 돌아가 있음. 내가 있던 페이지 번호로 돌아가게 할 수 있나 (위와 같이 History API를 이용한 이전 URL 추적으로 해결)

--- src/main/resources/static/js/community/readPostInit.js
+++ src/main/resources/static/js/community/readPostInit.js
@@ -1,8 +1,35 @@
 $(document).ready(function() {
-    // 뒤로 가기 버튼
-    $('.back-arrow').click(function() {
-        const board = $('#board').val();
-        location.href = `board?name=${board}`
+    /**
+     * 뒤로가기 버튼 이벤트 처리
+     * - 이전 페이지가 글쓰기('/writePost')이면, 해당 게시판의 첫 페이지로 이동
+     * - 이전 페이지가 글쓰기('/writePost')가 아니고 이전 페이지 이력이 있다면 , 검색 상태가 유지된 채로 뒤로가기
+     */
+    $('.back-arrow').click(function(e) {
+        e.preventDefault(); // <a> 태그의 기본 링크 이동 동작 방지
+
+        const referrer = document.referrer;
+        const boardName = $('#board').val(); // hidden input에서 게시판 이름 가져오기
+
+        // 1. 이전 페이지 URL(referrer) 정보가 있는지 확인
+        if (referrer) {
+            const previousUrl = new URL(referrer);
+
+            // 이전 페이지가 글쓰기 페이지('/writePost')인 경우
+            if (previousUrl.pathname.includes('/writePost')) {
+                // 해당 게시판의 기본 목록 페이지로 이동
+                location.href = `board?name=${boardName}`;
+            }
+            // 4. 그 외 다른 페이지에서 넘어온 경우
+            else {
+                // 일반적인 뒤로가기 동작 수행
+                history.back();
+            }
+        }
+        // 5. Referrer 정보가 없는 경우 (북마크나 URL 직접 입력으로 접속한 경우)
+        else {
+            // 기본 동작으로 게시판 목록으로 이동
+            location.href = `board?name=${boardName}`;
+        }
     });
 
     // 댓글 목록 가져오기
