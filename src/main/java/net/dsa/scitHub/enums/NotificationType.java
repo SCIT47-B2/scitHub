41eece61e3fcfd7f1c8790ea9845f27c1b3d4651
kumauma <sehyun1124@naver.com>
2025-09-30 12:45:36 +0900

    feat: 알림 기능 개선 및 공지사항 추가

--- src/main/java/net/dsa/scitHub/enums/NotificationType.java
+++ src/main/java/net/dsa/scitHub/enums/NotificationType.java
@@ -9,7 +9,8 @@ public enum NotificationType {
     NEW_LIKE_ON_POST("あなたの投稿に新しい「いいね！」が付きました。"),
     NEW_COMMENT_ON_POST("あなたの投稿に新しいコメントがありました。"),
     NEW_MESSAGE("新しいメッセージが届きました。"),
-    NEW_EVENT("全体スケジュールが登録されました。");
+    NEW_EVENT("全体スケジュールが登録されました。"),
+    NEW_ANNOUNCEMENT("新しいお知らせが投稿されました。");
 
     private final String title;
 }
